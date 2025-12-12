de4beb0c3338aa5c940746698bd33e78b98532d2
bhumireddysurekha7 <bhumireddysurekha7@gmail.com>
2025-12-12 12:48:56 +0530

    Update git clone command for web app deployment

--- scripts.sh
+++ scripts.sh
@@ -4,5 +4,5 @@ sudo apt update -y
 sudo apt install zip unzip -y
 sudo apt install nginx -y
 sudo rm -rf /var/www/html
-sudo git clone  /var/www/html
+sudo git clone https://github.com/bhumireddysurekha7/login-2510.git /var/www/html
 echo "Web Application Deployment Completed"
